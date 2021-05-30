b1a8853c9838f7f9fccbae4f90e035c4099167df
jakipatryk <18032838+jakipatryk@users.noreply.github.com>
2021-05-30 23:35:32 +0200

    Implement partitioner to use in iteration steps of PH

---
+++ src/main/scala/com/jakipatryk/spark/persistenthomology/IterationPartitioner.scala
@@ -0,0 +1,17 @@
+package com.jakipatryk.spark.persistenthomology
+
+import org.apache.spark.Partitioner
+
+class IterationPartitioner(val numPartitions: Int, val filtrationLength: Long) extends Partitioner {
+
+  /**
+   * Partitioner used during iteration of PH algorithm.
+   * Assumes there are no zero columns (they should be filtered out before repartitioning).
+   */
+  override def getPartition(key: Any): Int = key match {
+    case Key(_, Some(pivot)) => Math.floor(
+      pivot / Math.ceil(filtrationLength.toFloat / numPartitions)
+    ).toInt
+  }
+  
+}
