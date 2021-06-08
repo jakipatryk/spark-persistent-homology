9b6ebfda64de2d09db8e00bec6747f11bc4617df
jakipatryk <18032838+jakipatryk@users.noreply.github.com>
2021-06-08 22:00:54 +0200

    Add some utils for Chain class and a companion object

--- src/main/scala/com/jakipatryk/spark/persistenthomology/Chain.scala
+++ src/main/scala/com/jakipatryk/spark/persistenthomology/Chain.scala
@@ -1,9 +1,26 @@
 package com.jakipatryk.spark.persistenthomology
 
-class Chain(val asVector: SparseVectorMod2) {
+class Chain(val asVector: SparseVectorMod2) extends Serializable {
   def pivot: Option[Long] = asVector.first
 
   def +(other: Chain): Chain = new Chain(asVector + other.asVector)
+
+  override def toString: String = s"Chain(IndicesOfOnes(${asVector.toString}))"
+
+  def isEmpty: Boolean = asVector.isEmpty
+
+  override def equals(obj: Any): Boolean = obj match {
+    case c: Chain => asVector == c.asVector
+    case _ => false
+  }
+}
+
+object Chain {
+
+  def apply(asVector: SparseVectorMod2): Chain = new Chain(asVector)
+
+  def apply(asListOfOnes: List[Long]) = new Chain(new SparseVectorMod2(asListOfOnes))
+
 }
 
 case class Key(indexInMatrix: Long, pivot: Option[Long])
