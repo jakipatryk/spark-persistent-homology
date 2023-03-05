6349c5ff3f0374d1887a5457ec916755d88643a1
jakipatryk <bajbartlomiej997@gmail.com>
2023-03-05 21:52:41 +0100

    Finish CombinatorialNumberSystemOnSteroids implementation

--- src/main/scala/io/github/jakipatryk/sparkpersistenthomology/utils/CombinatorialNumberSystemOnSteroids.scala
+++ src/main/scala/io/github/jakipatryk/sparkpersistenthomology/utils/CombinatorialNumberSystemOnSteroids.scala
@@ -1,8 +1,11 @@
 package io.github.jakipatryk.sparkpersistenthomology.utils
 
-import scala.collection.Searching.{Found, InsertionPoint}
-
-
+/**
+ * Implementation of concatenation of Combinatorial Number Systems up to `maxCombinationSize`.
+ *
+ * @param combinationElementsSetSize Number of elements of the (ordered) multiset for which combinations are produced.
+ * @param maxCombinationSize Concatenate CNSs up to this, so included are sizes: 1, 2, ..., `maxCombinationSize`
+ */
 class CombinatorialNumberSystemOnSteroids(
                                            combinationElementsSetSize: Int,
                                            maxCombinationSize: Int
@@ -33,26 +36,53 @@ class CombinatorialNumberSystemOnSteroids(
     runningCounts
   }
 
+  /**
+   * A count of all combinations of `combinationElementsSetSize` elements up to `maxCombinationSize`-combination.
+   *
+   * In other words, if we let n := combinationElementsSetSize and k := maxCombinationSize:
+   * (n choose 1) + (n choose 2) + ... + (n choose k)
+   */
+  def allCombinationsCount: Long = combinationsRunningCounts.last
+
+  /**
+   * Computes combination (actually indices of elements in the combination)
+   * for an index in (global, meaning that in concatenated and limited to `maxCombinationSize`)
+   * lexicographical order of combinations.
+   *
+   * For `maxCombinationSize = 1` lexicographical order of combinations is trivial:
+   * (0), (1), ..., (combinationElementsSetSize)
+   *
+   * Then for `maxCombinationSize = 2` it is:
+   * (0), (1), ..., (combinationElementsSetSize),
+   * (1, 0), (2, 0), (2, 1), (3, 0), ..., (combinationElementsSetSize, combinationElementsSetSize - 1)
+   *
+   * And so on.
+   *
+   * @example
+   * val system = new CombinatorialNumberSystemOnSteroids(10, 2)
+   * system.getCombinationFromGlobalIndexInLexicographicalOrder(0) //  (0)
+   * system.getCombinationFromGlobalIndexInLexicographicalOrder(5) //  (5)
+   * system.getCombinationFromGlobalIndexInLexicographicalOrder(10) // (1, 0)
+   */
   def getCombinationFromGlobalIndexInLexicographicalOrder(index: Long): Array[Int] = {
-    import scala.collection.Searching.SearchImpl
+    import scala.collection.Searching._
 
     val correspondingCombinationSize = getCombinationSizeFromGlobalIndexInLexicographicalOrder(index)
     val indexWithinCorrespondingCombinationSizeGroup =
       index - combinationsRunningCounts(correspondingCombinationSize - 1)
-    // todo find combination with the greedy algorithm
+
     val combination = Array.fill(correspondingCombinationSize)(0)
     var currentTarget = indexWithinCorrespondingCombinationSizeGroup
 
     for (i <- 0 until correspondingCombinationSize) {
       val k = correspondingCombinationSize - i
       val kChooseValues = kthColumnOfCombinationsLookup(k)
-      val searchImpl = new SearchImpl[Long, IndexedSeq[Long]](kChooseValues)
       val orderingSkippingZeros: Ordering[Long] = (x: Long, y: Long) =>
         if(x < y) -1
         else if(x == y && y == 0) 1
         else if(x == y) 0
         else 1
-      val ithIndex = searchImpl.search(currentTarget)(orderingSkippingZeros) match {
+      val ithIndex = kChooseValues.search(currentTarget)(orderingSkippingZeros) match {
         case Found(foundIndex) => foundIndex
         case InsertionPoint(insertionPoint) => insertionPoint - 1
       }
@@ -63,13 +93,16 @@ class CombinatorialNumberSystemOnSteroids(
     combination
   }
 
+  /**
+   * Returns an iterator of combinations in global lexicographical order starting with one with index `startIndex`.
+   */
   def combinationsIterator(startIndex: Long): Iterator[Array[Int]] = new Iterator[Array[Int]] {
     import scala.util.control.Breaks._
 
     private var currentIndex = startIndex - 1
     private var currentCombination: Option[Array[Int]] = None
 
-    override def hasNext: Boolean = currentIndex + 1 < combinationsRunningCounts.last
+    override def hasNext: Boolean = currentIndex + 1 < allCombinationsCount
 
     override def next(): Array[Int] = {
       currentIndex += 1
@@ -116,13 +149,9 @@ class CombinatorialNumberSystemOnSteroids(
   }
 
   private def getCombinationSizeFromGlobalIndexInLexicographicalOrder(index: Long): Int = {
-    // TODO check why the following doesn't compile:
-    //    import scala.collection.Searching._
-    //    combinationsRunningCounts.search(index)
+    import scala.collection.Searching._
 
-    import scala.collection.Searching.SearchImpl
-    val searchImpl = new SearchImpl[Long, Array[Long]](combinationsRunningCounts)
-    searchImpl.search(index) match {
+    combinationsRunningCounts.search(index) match {
       case Found(foundIndex) => foundIndex + 1
       case InsertionPoint(insertionPoint) => insertionPoint
     }
@@ -134,10 +163,9 @@ class CombinatorialNumberSystemOnSteroids(
 
 }
 
-object CombinatorialNumberSystemOnSteroids extends App {
+object CombinatorialNumberSystemOnSteroids {
 
-  val c = new CombinatorialNumberSystemOnSteroids(10, 4)
-
-  c.combinationsIterator(0).foreach(a => println(a.mkString(", ")))
+  def apply(combinationElementsSetSize: Int, maxCombinationSize: Int): CombinatorialNumberSystemOnSteroids =
+    new CombinatorialNumberSystemOnSteroids(combinationElementsSetSize, maxCombinationSize)
 
 }
