b433c63dc4aa9fbe6d7793f9c3a7bd760136b872
Bartlomiej Baj <bajbartlomiej997@gmail.com>
2026-03-19 22:52:34 +0100

    Add the most important internal APIs

---
+++ src/main/scala/io/github/jakipatryk/sparkpersistenthomology/internal/vr/CoboundaryMatrixReducer.scala
@@ -0,0 +1,10 @@
+package io.github.jakipatryk.sparkpersistenthomology.internal.vr
+
+import org.apache.spark.sql.Dataset
+
+object CoboundaryMatrixReducer {
+
+  def reduce(coboundaryMatrix: Dataset[CoboundaryMatrixColumn]): Dataset[CoboundaryMatrixColumn] =
+    ???
+
+}
