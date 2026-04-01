410666978c779be6dc1f8c3a5782e9509af9c10b
Bartlomiej Baj <bajbartlomiej997@gmail.com>
2026-04-01 20:45:20 +0200

    Make `pivot` an `Option` as in case of 0 columns it is empty

--- src/main/scala/io/github/jakipatryk/sparkpersistenthomology/internal/vr/CoboundaryMatrixColumn.scala
+++ src/main/scala/io/github/jakipatryk/sparkpersistenthomology/internal/vr/CoboundaryMatrixColumn.scala
@@ -21,7 +21,7 @@ private[sparkpersistenthomology] case class CoboundaryMatrixColumn(
   valueTopEntries: Array[Simplex]
 ) {
 
-  @inline def pivot: Long = valueTopEntries(0).index
+  @inline def pivot: Option[Long] = valueTopEntries.headOption.map(_.index)
 
   /** Adds two CoboundaryMatrixColumn using fast addition on `valueTopEntries`. Falls back to full
     * resolution if the fast addition results in too few entries.
